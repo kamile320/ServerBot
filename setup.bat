35ae97a2929db15c743e950ea217f9062fbfd4e0
Krzysztof M <156579182+kamile320@users.noreply.github.com>
2025-12-26 15:55:32 +0100

    Update setup.bat
    - removed command adding ffmpeg files to Path (caused Path variable corruption)
    - added instruction how manually add FFmpeg .exe files to Path

--- setup.bat
+++ setup.bat
@@ -9,14 +9,18 @@ pip install -r Files\setup\requirements.txt
 
 pause
 
-@echo To use music commands you need to add FFmpeg files to PATH. You can download it here: 
+@echo To use music commands you have to add FFmpeg files to PATH. You can download it here: 
 @echo https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl-shared.zip
 @echo Extract files, rename folder to 'ffmpeg' and copy it to C:\ffmpeg
-@echo Now script will automatically add this files to local user PATH (C:\ffmpeg\bin). You should place '.exe' files of FFmpeg in the 'bin' folder. Add /M to add FFmpeg files to PATH for all users
 
 pause
 
-setx PATH "%PATH%;C:\ffmpeg\bin"
-@echo If adding to PATH was successful, you should now restart your computer.
+@echo Now you have to manually add FFmpeg files to local user PATH (C:\ffmpeg\bin). FFmpeg '.exe' files should be placed in the 'bin' folder.
+@echo In Windows GUI:
+@echo 1. Open Start Menu and search for 'Environment Variables' - select 'Edit the system environment variables'
+@echo 2. Select 'Path' variable in 'User variables' and click 'Edit'
+@echo 3. Click 'New' and add 'C:\ffmpeg\bin' (without quotes)
+@echo 4. Click OK to close all windows
+@echo 5. Restart your computer to apply changes
 
 pause
